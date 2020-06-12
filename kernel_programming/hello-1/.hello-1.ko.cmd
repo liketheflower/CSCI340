1f0516288841b2223d7f72a1e90f7f92a7361d02
osc <jim.morris.shen@gmail.com>
2020-06-12 14:42:14 -0600

    add tutorial about how to kernel programming

---
+++ kernel_programming/hello-1/.hello-1.ko.cmd
@@ -0,0 +1,1 @@
+cmd_/home/osc/repos/CSCI340/kernel_programming/hello-1/hello-1.ko := ld -r -m elf_x86_64 -T ./scripts/module-common.lds --build-id  -o /home/osc/repos/CSCI340/kernel_programming/hello-1/hello-1.ko /home/osc/repos/CSCI340/kernel_programming/hello-1/hello-1.o /home/osc/repos/CSCI340/kernel_programming/hello-1/hello-1.mod.o
---
+++ kernel_programming/hello-1/.tmp_versions/hello-1.mod
@@ -0,0 +1,2 @@
+/home/osc/repos/CSCI340/kernel_programming/hello-1/hello-1.ko
+/home/osc/repos/CSCI340/kernel_programming/hello-1/hello-1.o
